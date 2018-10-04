24e0e2fcbe6662681a006e9a519b25e3b63e3010
Kayra <kayraalat@gmail.com>
2018-10-04 18:26:52 +0100

    Added app

---
+++ client/app/app.jsx
@@ -0,0 +1,32 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface
+} from "react-apollo";
+
+import ChannelList from "./components/ChannelList/ChannelList";
+
+
+const createNetworkInterface = createNetworkInterface({
+  uri: "http://localhost:7700/graphql"
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+let app = document.querySelector("#app");
+
+render(
+  <ApolloProvider client={ client }>
+    <div className="App">
+      <h3 className="center">React, GraphQL, Apollo</h3>
+      <div className="row">
+        <div className="col-lg-4 col-lg-offset-4">
+          <ChannelList />
+        </div>
+      </div>
+    </div>
+  </ApolloProvider>,
+  app
+)
