9762443892c9cad920696d7fd3210b53f03c3859
Kayra <kayraalat@gmail.com>
2018-10-04 18:20:22 +0100

    Added channel list component

---
+++ client/app/ChannelList/ChannelList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { gql, graphql } from "react-apollo";
+
+
+const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+const ChannelsList = ({ data: { loading, error, channels }}) => {
+
+  if (loading) {
+    return <p>Fetching data...</p>;
+  }
+
+  if (error) {
+    return <p>{ error.message }</p>
+  }
+
+  const listOfChannels = (
+  <ul className="list-group">
+    
+    { channels.map( channel => 
+      <li class="list-group-item" key={channel.id}>
+        { channel.name }
+      </li>)}
+      
+  </ul>);
+
+  return listOfChannels
+
+}
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+export default ChannelsListWithData;
